2915a693032592cf9885e9e77d1dedd5d9141c1b
Atilim Gunes Baydin <atilimgunes.baydin@gmail.com>
2018-09-19 04:19:22 +0100

    Update example

--- src/pyprob_cpp/test/test_set_defaults_and_addresses.cpp
+++ src/pyprob_cpp/test/test_set_defaults_and_addresses.cpp
@@ -9,24 +9,30 @@ xt::xarray<double> forward()
 
   pyprob_cpp::setDefaultControl(true);
   pyprob_cpp::setDefaultReplace(false);
-
   auto normal1 = pyprob_cpp::distributions::Normal(prior_mean, prior_stddev);
-  auto mu1 = pyprob_cpp::sample(normal1, "mu1");
-  mu1      = pyprob_cpp::sample(normal1, "mu1");
+  xt::xarray<double> mu1;
+  for (int i = 0; i < 2; i++)
+  {
+     mu1 = pyprob_cpp::sample(normal1, "mu1");
+  }
 
   pyprob_cpp::setDefaultControl(true);
   pyprob_cpp::setDefaultReplace(true);
-
   auto normal2 = pyprob_cpp::distributions::Normal(mu1, prior_stddev);
-  auto mu2 = pyprob_cpp::sample(normal2, "mu2");
-  mu2      = pyprob_cpp::sample(normal2, "mu2");
+  xt::xarray<double> mu2;
+  for (int i = 0; i < 2; i++)
+  {
+    mu2 = pyprob_cpp::sample(normal2, "mu2");
+  }
 
   pyprob_cpp::setDefaultControl(false);
   pyprob_cpp::setDefaultReplace(false);
-
   auto normal3 = pyprob_cpp::distributions::Normal(mu2, prior_stddev);
-  auto mu3 = pyprob_cpp::sample(normal3, "mu3");
-  mu3      = pyprob_cpp::sample(normal3, "mu3");
+  xt::xarray<double> mu3;
+  for (int i = 0; i < 2; i++)
+  {
+    mu3 = pyprob_cpp::sample(normal3, "mu3");
+  }
 
   auto likelihood = pyprob_cpp::distributions::Normal(mu3, likelihood_stddev);
   pyprob_cpp::observe(likelihood, "obs");
